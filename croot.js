01c24af6719b9b49e2ec7dc2641dd9581f4a97c4
Gading Khairlambang <sasakihaise985@gmail.com>
2023-10-12 14:18:27 +0700

    croot

--- croot.js
+++ croot.js
@@ -7,3 +7,13 @@ onClick("tombol",aktif)
 function aktif(){
     alert("udah aktif")
 }
+
+import { container } from "https://jscroot.github.io/element/croot.js"
+function ubahTeks(){
+    var elemen = container("myDiv")
+    if (elemen) {
+        elemen.textContent = "Teks ini telah diubah dengan JavaScript!";
+    }
+}
+
+document.getElementById("klik").addEventListener("click", ubahTeks)
