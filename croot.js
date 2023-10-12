a63fab5829c99b609b6cd171a9b51816ec6888d7
Gading Khairlambang <sasakihaise985@gmail.com>
2023-10-12 14:13:00 +0700

    js

--- croot.js
+++ croot.js
@@ -1,19 +1,9 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-setInner("demo", "Gading Khairlambang");
+setInner("demo","Gading Khairlambang");
 
 import { onClick } from "https://jscroot.github.io/element/croot.js";
-onClick("tombol", aktif);
+onClick("tombol",aktif)
 
-function aktif() {
-  alert("udah aktif");
+function aktif(){
+    alert("udah aktif")
 }
-
-// import { container } from "https://jscroot.github.io/element/croot.js";
-// function container() {
-//   var elemen = container("myDiv");
-//   if (element) {
-//     element.textContent = "Teks ini telah diubah dengan JavaScript!";
-//   }
-// }
-
-// document.getElementById("klik").addEventListener("click", container);
