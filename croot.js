1ef65347d37afa3c76805baf9df990ab430fb2a9
Gading Khairlambang <sasakihaise985@gmail.com>
2023-10-12 09:41:01 +0700

    js

--- croot.js
+++ croot.js
@@ -2,4 +2,8 @@ import { setInner } from "https://jscroot.github.io/element/croot.js";
 setInner("demo","Dari croot.js");
 
 import { onClick } from "https://jscroot.github.io/element/croot.js";
-onClick("tombol","aktif")
+onClick("tombol",aktif)
+
+function aktif(){
+    alert("udah aktif")
+}
